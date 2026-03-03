fee6562f1490a8686aa4ff3ae247673d5ec16ed8
Lucas Nahuel Devecchi Di Bella <ldevecchi@frba.utn.edu.ar>
2026-03-02 22:35:55 -0300

    feat: created functions to handle different types of controllers

---
+++ backend/src/delivery/utils/Functions.ts
@@ -0,0 +1,66 @@
+import { Request } from "express";
+import { BusinessId } from "../../modules/businesses/core/domain/Business";
+import { BadRequest } from "../../modules/shared/domain/errors/BadRequest";
+import { Pagination } from "../../modules/shared/domain/Pagination";
+
+
+export enum PathParams {
+    BUSINESS_ID = 'businessId',
+}
+
+const getOptionalPathParam = (req: Request, param: PathParams): string | undefined => {
+    return req.params[param];
+};
+
+const getPathParam = (req: Request, param: PathParams): string => {
+    const value = getOptionalPathParam(req, param);
+    if (!value) {
+        throw new BadRequest(`${param} not found`);
+    }
+    return value;
+};
+
+// const getIntPathParam = (req: Request, param: PathParams): number => {
+//     const value = getPathParam(req, param);
+//     const parsed = parseInt(value);
+//     if (isNaN(parsed)) {
+//         throw new BadRequest(`${param} is not a number`);
+//     }
+//     return parsed;
+// };
+
+export const getBusinessId = (req: Request): BusinessId => {
+    return getPathParam(req, PathParams.BUSINESS_ID);
+};
+
+export const getPage = (req: Request): number | undefined => {
+    if (!req.query.page) {
+        return undefined;
+    }
+    const page = parseInt(req.query.page as string);
+    if (isNaN(page)) {
+        throw new BadRequest('Invalid page');
+    }
+    return page;
+};
+
+export const getPageSize = (req: Request): number | undefined => {
+    if (!req.query.pageSize) {
+        return undefined;
+    }
+    const pageSize = parseInt(req.query.pageSize as string);
+    if (isNaN(pageSize)) {
+        throw new BadRequest('Invalid page size');
+    }
+    return pageSize;
+};
+
+
+export const getPagination = (req: Request): Pagination => {
+    return Pagination.default({
+        page: getPage(req),
+        pageSize: getPageSize(req)
+    });
+};
+
+// TODO: handle images and roles
