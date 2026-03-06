03071d4854deaf7c14fadec0e3477988e56feb48
Lucas Nahuel Devecchi Di Bella <ldevecchi@frba.utn.edu.ar>
2026-03-06 08:28:45 -0300

    feat: update TypeORMAccountRepository and created TypeORMGroupRepository

---
+++ backend/src/modules/accounts/infrastructure/repositories/TypeormGroupRepository.ts
@@ -0,0 +1,42 @@
+import { DataSource, Repository } from 'typeorm';
+import { GroupRepository } from '../../core/domain/GroupRepository';
+import { Group, GroupId } from '../../core/domain/Group';
+import { TypeormGroup } from '../entities/TypeormGroup';
+import { AccountId } from '../../core/domain/Account';
+import { GroupNotFound } from '../../core/domain/errors/SignerGroupNotFound';
+
+export class TypeormGroupRepository implements GroupRepository {
+    private repository: Repository<TypeormGroup>;
+
+    constructor(dataSource: DataSource) {
+        this.repository = dataSource.getRepository(TypeormGroup);
+    }
+
+    async save(group: Group): Promise<Group> {
+        const entity = TypeormGroup.fromDomain(group);
+        await this.repository.save(entity);
+        return entity.toDomain();
+    }
+
+    async getById(id: GroupId): Promise<Group | undefined> {
+        const group = await this.repository.findOneBy({ id });
+        return group ? group.toDomain() : undefined;
+    }
+
+    async getByAccountId(accountId: AccountId): Promise<Group[]> {
+        const groups = await this.repository.find({
+            where: { accountId },
+            order: { name: 'ASC' },
+        });
+        return groups.map((sg) => sg.toDomain());
+    }
+
+    async delete(id: GroupId): Promise<Group> {
+        const group = await this.repository.findOneBy({ id });
+        if (!group) {
+            throw new GroupNotFound(id);
+        }
+        await this.repository.delete(id);
+        return group.toDomain();
+    }
+}
